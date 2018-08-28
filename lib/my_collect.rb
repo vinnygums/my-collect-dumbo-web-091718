b6d406136a7c958e047e322b75b8e4a14f60db85
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-28 02:05:26 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,8 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-   new_array = new_array + yield array[i]
+   new_array = yield array[i]
    i = i+ 1
   end
-  new_array
 end
 
