faa0049ef6114fd4f83a3b8b14e703f8247ba476
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-28 02:12:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+  i = 0
+  another_array = []
+  while i < array.length
+   new_array = yield array[i]
+   another_array << new_array
+   i = i+ 1
+  end
+  another_array
+end
 
